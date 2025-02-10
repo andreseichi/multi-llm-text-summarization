bd5264540c43697356586e1e1c340b6ffbd08f3d
tashiro <andre_seichi@hotmail.com>
2025-02-10 16:15:04 -0300

    multi-llm centralizado e descentralizado

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import { loadTextFromFile } from "./loadText";
 import { MultiLLMSummarizerCentralized } from "./MultiLLMSummarizerCentralized";
+import { MultiLLMSummarizerDecentralized } from "./MultiLLMSummarizerDecentralized";
 
-const textPath = './src/text.txt'
+const textPath = "./src/texts/text2.txt";
 
+const models = ["llama3.1", "llama2", "stablelm2"];
 const main = async (): Promise<void> => {
-  const models = ["llama3.1", "llama3.2"];
-
-  const summarizer = new MultiLLMSummarizerCentralized(models);
-
   const text = await loadTextFromFile(textPath);
+
+  console.log("Topologia Centralizada");
+  const summarizer = new MultiLLMSummarizerCentralized(models);
 
   const results = await summarizer.generateSummaries(text);
 
-  console.log({ results });
+  await summarizer.evaluateSummaries(text, results);
 
-  const evaluatedSummaries = await summarizer.evaluateSummaries(text, results);
+  console.log("Topologia Descentralizada");
+  const summarizerDecentralized = new MultiLLMSummarizerDecentralized(models);
 
-  console.log({ evaluatedSummaries });
+  await summarizerDecentralized.summarizeWithConsensus(text);
 };
 
 main();
